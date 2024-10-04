e6c895f8be5b5b3be98d86eb26a53c75f6d842ca
nakanishil <nakanishi.jsdf@gmail.com>
2024-10-05 00:30:50 +0900

    kadai_mdn_jsの追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+// Date()から年、月、日を抜き取り定数に代入する
+const year = new Date().getFullYear();
+const month = new Date().getMonth();
+const day = new Date().getDate();
+
+// 課題に適した状態で出力する
+console.log(year + '年' + month + '月' + day + '日');
+
