ff10bdb440119651988cfc8015f569ef938950f7
nakanishil <nakanishi.jsdf@gmail.com>
2024-10-06 00:23:00 +0900

    kadai_mdn_jdの訂正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
 // Date()から年、月、日を抜き取り定数に代入する
 const year = new Date().getFullYear();
-const month = new Date().getMonth();
+const month = new Date().getMonth() + 1;
 const day = new Date().getDate();
 
 // 課題に適した状態で出力する
