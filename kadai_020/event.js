b7da05d09ec04637200a6529bac637723bd954de
nakanishil <nakanishi.jsdf@gmail.com>
2024-10-07 10:59:05 +0900

    kadai_020の編集

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,6 +1,7 @@
 // html要素をidで取得
 const btn = document.getElementById('btn')
 
+const text =document.getElementById('text');
 // btnがクリックされたときにイベント処理を実行
 btn.addEventListener('click', () => {
   // id textの書き換え
